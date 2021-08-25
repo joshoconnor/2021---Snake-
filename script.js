212d1f3c6a874449ecae164497da76e32ce9b36c
joshoconnor1993@gmail.com <joshoconnor1993@gmail.com>
2021-08-25 20:19:47 +0100

    made the code way more ugly but added the ability to identify the head, body, corners, and tail

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const playArea = document.querySelector("#playArea");
-const gridWidth = 20;
-const gameSpeed = 150;
+const gridWidth = 10;
+const gameSpeed = 200;
 const snake = [];
 const directionalKeys = {
     "d" : ["left", "right"],
@@ -37,10 +37,18 @@ startButton.addEventListener("click", () => {
 const startGame = () => {
     score = 0;
     snake.length = 0;
-    snake.unshift(
-        [gridWidth / 2, gridWidth / 2],
-        [gridWidth / 2, (gridWidth / 2) - 1]
-    );
+    snake.unshift({
+        y: gridWidth / 2,
+        x: gridWidth / 2,
+        direction: proposedDirection,
+        bodyPart: "head"
+    },
+    {
+        y: gridWidth / 2,
+        x: (gridWidth / 2) -1,
+        direction: proposedDirection,
+        bodyPart: "tail"
+    });
     let fruit = playArea.querySelector(".fruit");
     if(fruit != undefined) {fruit.classList.remove("fruit")};
     renderSnake();
@@ -60,44 +68,100 @@ const renderSnake = () => {
     // clears the snake from the board
     let currentPixels = playArea.querySelectorAll(".snake");
     currentPixels.forEach((currentSnakePixel) => {
-        currentSnakePixel.classList.remove("snake");
+        currentSnakePixel.classList.remove("snake", "left", "right", "up", "down", "head", "tail", "body", "corner-oiio", "corner-iioo", "corner-ooii", "corner-iooi");
     });
-    let currentSnakeHead = playArea.querySelector(".head");
-    if (currentSnakeHead != undefined) {currentSnakeHead.classList.remove("head");}
-
     // adds the snake from the array to the board
     snake.forEach((section) => {
-        let newSnakePixel = document.querySelector(`div[y="${section[0]}"][x="${section[1]}"]`);
-        newSnakePixel.classList.add("snake");
+        let newSnakePixel = document.querySelector(`div[y="${section.y}"][x="${section.x}"]`);
+        newSnakePixel.classList.add("snake", `${section.direction}`, `${section.bodyPart}`);
     });
-    let newSnakeHead = document.querySelector(`div[y="${snake[0][0]}"][x="${snake[0][1]}"]`);
-    newSnakeHead.classList.add("head");
 };
 
 // Moves the snake
 const moveSnake = () => {
     // adds a copy of the head to the front of the snake array
-    snake.unshift(snake[0].slice());
+    snake.unshift({
+        y: snake[0].y,
+        x: snake[0].x,
+        direction: proposedDirection,
+        bodyPart: "head"
+    });
+    if(snake.length >= 4) {whichBodyPart();};
     // sets the coordinates of the head based on the current direction and postition of the snake
     switch(proposedDirection) {
         case "right":
-            snake[0][1] == gridWidth ? snake[0][1] = 1 : ++snake[0][1];
+            snake[0].x == gridWidth ? snake[0].x = 1 : ++snake[0].x;
             lastDirection = "right";
         break;
         case "left":
-            snake[0][1] == 1 ? snake[0][1] = gridWidth : --snake[0][1];
+            snake[0].x == 1 ? snake[0].x = gridWidth : --snake[0].x;
             lastDirection = "left";
         break;
         case "up":
-            snake[0][0] == 1 ? snake[0][0] = gridWidth : --snake[0][0];
+            snake[0].y == 1 ? snake[0].y = gridWidth : --snake[0].y;
             lastDirection = "up";
         break;
         case "down":
-            snake[0][0] == gridWidth ? snake[0][0] = 1 : ++snake[0][0];
+            snake[0].y == gridWidth ? snake[0].y = 1 : ++snake[0].y;
             lastDirection = "down";
     };
     // extends the snake if its eating
-    snakeEating == "false" ? snake.pop() : snakeEating = "false";
+    if(snakeEating == "false") {
+        snake.pop();
+        snake[(snake.length) - 1].bodyPart = "tail";
+        snake[(snake.length) - 1].direction = snake[(snake.length) - 2].direction;
+    } else {
+        snakeEating = "false";
+    };
+};
+
+// works out what the body part of the section behind the snake's head is
+const whichBodyPart = () => {
+    if(snake[1].direction == snake[0].direction) {
+        snake[1].bodyPart = "body"
+    } else {
+        switch(snake[1].direction) {
+            case "right":
+                switch(snake[0].direction) {
+                    case "up":
+                        snake[1].bodyPart = "corner-oiio"
+                    break;
+                    case "down":
+                        snake[1].bodyPart = "corner-iioo"
+                    break;
+                }
+            break;
+            case "left":
+                switch(snake[0].direction) {
+                    case "up":
+                        snake[1].bodyPart = "corner-ooii"
+                    break;
+                    case "down":
+                        snake[1].bodyPart = "corner-iooi"
+                    break;
+                }
+            break;
+            case "up":
+                switch(snake[0].direction) {
+                    case "right":
+                        snake[1].bodyPart = "corner-iooi"
+                    break;
+                    case "left":
+                        snake[1].bodyPart = "corner-iioo"
+                    break;
+                }
+            break;
+            case "down":
+                switch(snake[0].direction) {
+                    case "right":
+                        snake[1].bodyPart = "corner-ooii"
+                    break;
+                    case "left":
+                        snake[1].bodyPart = "corner-oiio"
+                    break;
+                };
+        };
+    };
 };
 
 // Place fruit randomly in a square on the board which isn't where the snake is
@@ -110,6 +174,7 @@ const placeFruit = () => {
 
 // Checks for directional button changes
 document.addEventListener('keydown', (e) => {
+    if(!directionalKeys[e.key]) return;
     if(lastDirection != directionalKeys[e.key][0]) {
         proposedDirection = directionalKeys[e.key][1]
     };
@@ -128,7 +193,12 @@ const checkFruitEaten = () => {
 
 // Checks if the snake bites itself by checking for duplicates in the array
 const checkSelfSnakeBite = () => {
-    let countDuplicates = snake.length - new Set( snake.map(JSON.stringify) ).size;
+    let snakeCoordinates = []
+    snake.forEach(section => {
+        sectionArray = [section.x, section.y];
+        snakeCoordinates.unshift(sectionArray);
+    });
+    let countDuplicates = snakeCoordinates.length - new Set( snakeCoordinates.map(JSON.stringify) ).size;
     countDuplicates <= 0 ? renderSnake() : gameStatus = endGame();
 };
 
